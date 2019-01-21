3dfac45a730ee860c10b2f269809c26fc93c2308
Jacek Janczura <janczura.jacek@gmail.com>
2019-01-21 19:08:44 +0100

    correct bootnode config - bootnode ip

--- ethereum/console.sh
+++ ethereum/console.sh
@@ -11,6 +11,6 @@ fi
 BOOTNODEID=$(cat ./boot.key)
 
 # TODO: Get the public IP of the bootnode from its hostname
-BOOTNODEIP="18.194.232.161"
+BOOTNODEIP="18.195.130.226"
 
 geth --datadir "./node" --preload "./js/lib.js" --syncmode "full" --port 30300 --rpc --rpcport 8500 --rpcapi 'personal,db,eth,net,web3,txpool,miner' --bootnodes "enode://$BOOTNODEID@$BOOTNODEIP:30310" --networkid 42 --gasprice "1" --etherbase "$KEY" --unlock "$KEY" --password "./password.txt" console
