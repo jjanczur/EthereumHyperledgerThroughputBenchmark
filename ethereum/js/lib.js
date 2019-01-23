f0b2a3340400aa9384f9fd0b011413c9c054e624
Igor Molcean <molcean@campus.tu-berlin.de>
2019-01-23 16:01:40 +0100

    Node indexes corrected

--- ethereum/js/lib.js
+++ ethereum/js/lib.js
@@ -1,7 +1,7 @@
 // Account list
 
-var node0 = "0x909913a250c7087d426c20b46b913d4dd9b526a8";
-var node1 = "0x42246e58c85037f28177254f352fee4f2bd792ff";
+var node1 = "0x909913a250c7087d426c20b46b913d4dd9b526a8";
+var node2 = "0x42246e58c85037f28177254f352fee4f2bd792ff";
 
 
 // Some function to make life easier
@@ -13,37 +13,19 @@ function getBalance(acc)
 
 function send(value)
 {
-	if (eth.coinbase==node0) {
+	if (eth.coinbase==node1) {
 			return eth.sendTransaction({
         		"from" : eth.coinbase,
-        		"to" : node1,
+        		"to" : node2,
         		"value" : web3.toWei(value, "ether")
         	});
 	} else {
 		 	return eth.sendTransaction({
        			"from" : eth.coinbase,
-        		"to" : node0,
+        		"to" : node1,
         		"value" : web3.toWei(value, "ether")
        		});
 	}
-}
-
-function sendToNode0(value)
-{
-    return eth.sendTransaction({
-        "from" : eth.coinbase,
-        "to" : node0,
-        "value" : web3.toWei(value, "ether")
-        });
-}
-
-function sendToNode1(value)
-{
-    return eth.sendTransaction({
-        "from" : eth.coinbase,
-        "to" : node1,
-        "value" : web3.toWei(value, "ether")
-        });
 }
 
 function sendTransaction(to, value)
