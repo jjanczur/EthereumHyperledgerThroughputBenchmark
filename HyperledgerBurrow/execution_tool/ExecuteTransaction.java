2aeb41a0857fb630b66046e57537ea487688feac
Julian Weigel <valewei@mailbox.tu-berlin.de>
2019-01-25 14:16:46 +0100

    Changed execution tool and added better outbut json file for testing

--- HyperledgerBurrow/execution_tool/ExecuteTransaction.java
+++ HyperledgerBurrow/execution_tool/ExecuteTransaction.java
@@ -1,51 +1,58 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.stream.Collectors;
 
 public class ExecuteTransaction
 {
+    /* Nodes */
     public static final String validator = "4474D57D0F6C70FEBDA3D0B0D602A64F68134A0F";
     public static final String participant = "AAE79FCC1051D8921723D66272CA49B42B044C83";
+    
+    /* Commands */
+    public static String CmdStartNode = " start --validator-index=0";
+    public static String CmdTransaction = " deploy --address " + validator + " -f test.yaml";
+    
+    /** Usage java ExecuteTransaction path/to/burrow_executable */
     public static void main(String[] args)
     {
-        /*
-        long l = 564897;
-        String st = ""+l;
-        for(int i = st.length(); i < 9; i++)
+        if(args.length != 1)
         {
-            st = "0" + st;
+            System.out.println("Missing parameters. java ExecuteTransaction path/to/burrow_executable");
+            System.exit(0);
         }
-        st = "0." + st;
-        double d = Double.parseDouble(st);
-        System.out.println(d);
-        System.exit(0);
-        */
-        String CmdStartNode = "./burrow start --validator-index=0";
-        String CmdTransaction = "./burrow deploy --address " + validator + " -f test.yaml";
+        CmdTransaction = args[0] + CmdTransaction;
+        //System.out.println(CmdTransaction);
         try
         {
-            String s = "";
-            Process p = Runtime.getRuntime().exec(CmdTransaction);
-            BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
-            BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-
-            long time = System.currentTimeMillis();
-            while ((s = stdError.readLine()) != null)
+            for(int i = 0; i < 20; i++)
             {
-                if(s.startsWith("Transaction Hash"))
+                String s = "";
+                Process p = Runtime.getRuntime().exec(CmdTransaction);
+                BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+                
+                long time = System.currentTimeMillis();
+                System.out.println(p);
+                
+                while ((s = stdError.readLine()) != null)
                 {
-                    time = System.currentTimeMillis() - time;
-                    System.out.println(time + "");
-                    int idx = s.indexOf("=>") + 3;
-                    System.out.println("" + s.substring(idx, s.length()-1) + " " + time + "ms");
+                    if(s.startsWith("Transaction Hash"))
+                    {
+                        time = System.currentTimeMillis() - time;
+                        System.out.println(time + "");
+                        int idx = s.indexOf("=>") + 3;
+                        System.out.println("" + s.substring(idx, s.length()-1) + " " + time + "ms");
+                    }
                 }
+                
+                p.waitFor();
+                System.out.println ("exit: " + p.exitValue());
+                p.destroy();
             }
-            p.waitFor();
-            System.out.println ("exit: " + p.exitValue());
-            p.destroy();
         }
         catch(Exception ex)
         {
-        
+            System.out.println(ex);
         }
     }
 }
