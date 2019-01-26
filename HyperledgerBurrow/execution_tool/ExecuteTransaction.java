35c3fef1b42d37c26a42c70030b8e65deca278eb
Julian Weigel <valewei@mailbox.tu-berlin.de>
2019-01-26 12:30:21 +0100

    Had to add a threading pool, because of the tcp messages taking too much time to finish end would result in a wrong throughput calculation

--- HyperledgerBurrow/execution_tool/ExecuteTransaction.java
+++ HyperledgerBurrow/execution_tool/ExecuteTransaction.java
@@ -1,62 +1,43 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.stream.Collectors;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
 
 public class ExecuteTransaction
 {
     /* Nodes */
-    public static String validator = "4474D57D0F6C70FEBDA3D0B0D602A64F68134A0F";
-    public static String participant = "AAE79FCC1051D8921723D66272CA49B42B044C83";
+    public static String validator = "";
+    public static String participant = "";
     
     /* Commands */
-    public static String CmdStartNode = " start --validator-index=0";
-    public static String CmdTransaction = " deploy --address " + validator + " -f test.yaml";
+    public static String cmdStartNode = " start --validator-index=0";
+    public static String cmdTransaction = " deploy --address " + validator + " -f test.yaml";
     
     /** Usage java ExecuteTransaction path/to/burrow_executable */
     public static void main(String[] args)
     {
-        if(args.length != 3)
+        if(args.length != 6)
         {
-            System.out.println("Missing parameters. java ExecuteTransaction path/to/burrow_executable validator_ID no_of_iterations");
+            System.out.println("Missing parameters. java ExecuteTransaction path/to/burrow_executable validator_ID no_of_iterations url port thread_pool_size");
             System.exit(0);
         }
         validator = args[1];
-        CmdTransaction = args[0] + " deploy --address " + validator + " -f test.yaml";;
-        System.out.println(CmdTransaction);
+        cmdTransaction = args[0] + " deploy --address " + validator + " -f test.yaml -u " + args[3] + ":" + args[4];
+        System.out.println(cmdTransaction);
         int n = Integer.parseInt(args[2]);
         
         long t0 = System.currentTimeMillis();
         System.out.println("Starting at time: " + t0);
-        try
+        
+        ExecutorService executor = Executors.newFixedThreadPool(Integer.parseInt(args[5]));
+        for(int i = 0; i < n; i++)
         {
-            for(int i = 0; i < n; i++)
-            {
-                String s = "";
-                Process p = Runtime.getRuntime().exec(CmdTransaction);
-                BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
-                BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-                
-                long time = System.currentTimeMillis();
-                
-                while ((s = stdError.readLine()) != null)
-                {
-                    if(s.startsWith("Transaction Hash"))
-                    {
-                        time = System.currentTimeMillis() - time;
-                        System.out.println(time + "");
-                        int idx = s.indexOf("=>") + 3;
-                        System.out.println("" + s.substring(idx, s.length()-1) + " " + time + "ms");
-                    }
-                }
-                
-                p.waitFor();
-                System.out.println ("exit: " + p.exitValue());
-                p.destroy();
-            }
+            Runnable worker = new Worker(cmdTransaction);
+            executor.execute(worker);
         }
-        catch(Exception ex)
+        executor.shutdown();
+        while(!executor.isTerminated())
         {
-            System.out.println(ex);
+            //wait for all threads
         }
+        System.out.println("Execution finished!");
     }
 }
