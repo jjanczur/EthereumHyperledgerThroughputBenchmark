f4e3061c45da10ee2698b1e5352b18542bd8acd8
valewei <julian.v.weigel@campus.tu-berlin.de>
2019-02-02 13:51:43 +0100

    Added command to build.sh to build the bibtex as well. Added my author tags and result for hyperledger

--- Report/build.sh
+++ Report/build.sh
@@ -1,1 +1,5 @@
+rm main.aux main.bbl main.blg main.log main.toc
+pdflatex -interaction=nonstopmode main.tex
+bibtex main.aux
+pdflatex -interaction=nonstopmode main.tex
 pdflatex -interaction=nonstopmode main.tex
